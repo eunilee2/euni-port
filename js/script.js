02fbe0ec3ae86444e415d2cc957c2d3b99c8cbb7
Eunice <115505438+eunilee2@users.noreply.github.com>
2023-04-12 03:53:48 -0400

    FP3

--- js/script.js
+++ js/script.js
@@ -1,46 +1,60 @@
-import * as THREE from 'three';
+import { WebGL } from 'three/addons/capabilities/WebGL.js';
 
-let scene, camera, renderer;
+if ( WebGL.isWebGLAvailable() ) {
 
-function init() {
-    //create scene object
-    scene = new THREE.Scene();
+	// Initiate function or other initializations here
+	animate();
+
+} else {
+
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
+
+}
+
+
+
+// let scene, camera, renderer;
+
+// function init() {
+//     //create scene object
+//     scene = new THREE.Scene();
     
-    //setup camera with facing upward
-    camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.z = 1;
-    camera.rotation.x = Math.PI/2;
+//     //setup camera with facing upward
+//     camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 1, 1000);
+//     camera.position.z = 1;
+//     camera.rotation.x = Math.PI/2;
     
-    //setup renderer
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+//     //setup renderer
+//     renderer = new THREE.WebGLRenderer();
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+//     document.body.appendChild(renderer.domElement);
 
-    animate(); 
-}
-//rendering loop
-function animate() {
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
-}
-init();
+//     animate(); 
+// }
+// //rendering loop
+// function animate() {
+//     renderer.render(scene, camera);
+//     requestAnimationFrame(animate);
+// }
+// init();
 
-starGeo = new THREE.Geometry();
-for(let i=0;i<6000;i++) {
-    let star = new THREE.Vector3(
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300
-    );
-    starGeo.vertices.push(star);
-}
+// starGeo = new THREE.Geometry();
+// for(let i=0;i<6000;i++) {
+//     let star = new THREE.Vector3(
+//         Math.random() * 600 - 300,
+//         Math.random() * 600 - 300,
+//         Math.random() * 600 - 300
+//     );
+//     starGeo.vertices.push(star);
+// }
 
-let sprite = new THREE.TextureLoader().load( 'star.png' );
-let starMaterial = new THREE.PointsMaterial({
-    color: 0xaaaaaa,
-    size: 0.7,
-    map: sprite
-});
+// let sprite = new THREE.TextureLoader().load( 'star.png' );
+// let starMaterial = new THREE.PointsMaterial({
+//     color: 0xaaaaaa,
+//     size: 0.7,
+//     map: sprite
+// });
 
-stars = new THREE.Points(starGeo,starMaterial);
-scene.add(stars);
+// stars = new THREE.Points(starGeo,starMaterial);
+// scene.add(stars);
