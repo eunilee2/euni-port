1229bb1b5c7133e3c7dd0cb5f0ba9563cf7bb050
Eunice <115505438+eunilee2@users.noreply.github.com>
2023-05-04 10:42:05 -0400

    FP3

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// function carouselButton() {
+//     if (x.matches) { 
+//         $(document).ready(function(){
+//             $('#carousel').slick({
+//                 dots: false,
+//                 infinite: true,
+//                 speed: 500,
+//                 fade: true,
+//                 cssEase: 'linear',
+//                 // prevArrow:true
+//                 // nextArrow:<button class="slick-next" aria-label="Next" type="button">▼</button>,
+//             });
+//         });
+//     } else {
+//         $(document).ready(function(){
+//             $('#carousel').slick({
+//                 dots: false,
+//                 infinite: true,
+//                 speed: 500,
+//                 fade: true,
+//                 cssEase: 'linear',
+//                 // prevArrow:<button class="slick-prev" aria-label="Previous" type="button">◄</button>,
+//                 // nextArrow:<button class="slick-next" aria-label="Next" type="button">►</button>,
+//             });
+//         });
+//     }
+//   }
+  
+//   var x = window.matchMedia("(max-width: 400px)")
+//   carouselButton(x) // Call listener function at run time
+//   x.addEventListener(carouselButton) // Attach listener function on state changes
