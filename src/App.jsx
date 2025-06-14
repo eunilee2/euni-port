5ec2ff4c80486edfeaa84b7badad97c7460dbb4e
Eunice <eylee2@andrew.cmu.edu>
2025-06-14 15:18:13 -0400

    fix routing for vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/home";
+import { Home } from "./pages/Home";
 import { IBM } from "./pages/IBM";
 import  { IBM2 } from "./pages/IBM2";
 import  { SLZ } from "./pages/SLZ";
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import autoprefixer from 'autoprefixer'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/euni-port",
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
